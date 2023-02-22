38c09eb37ec7c485be15dad561c8540ce56e96d2
CodeConscious <hello@codeconscio.us>
2023-02-22 19:07:56 +0900

    Add F# (#3)
    * Add F#
    
    * Add .gitignore
    
    * Remove Haskell folder

---
+++ fsharp/README.md
@@ -0,0 +1,7 @@
+# F# Dojo
+
+Playing around with F#! It's a lower priority at the moment, but I hope to toy around with it a bit here and there.
+
+## References
+
+- https://learn.microsoft.com/en-us/dotnet/fsharp/get-started/get-started-command-line
---
+++ fsharp/src/App/Program.fs
@@ -0,0 +1,14 @@
+﻿// Reference: https://aka.ms/fsharp-console-apps
+open System
+open Library
+
+[<EntryPoint>]
+let main args =
+    printfn "F#アプラを実行したぜ！"
+    printfn "System.Text.Json output regarding args:"
+
+    let value, json = getJson {| args=args; year=System.DateTime.Now.Year |}
+    printfn $"Input: %0A{value}"
+    printfn $"Output: %s{json}"
+
+    0
