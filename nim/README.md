a9d3bcecae00532a41772bddc54d5525091ad2fa
CodeConscious <hello@codeconscio.us>
2023-02-27 19:44:51 +0900

    Add new object (BROKEN BUILD)

--- nim/README.md
+++ nim/README.md
@@ -1,3 +1,5 @@
 # Nim
 
 [Nim](https://nim-lang.org/) seems pretty well liked, and seems to be something like a statically-typed Python.
+
+Run using `nim c -r main.nim`.
--- nim/main.nim
+++ nim/main.nim
@@ -1,19 +1,28 @@
 import std/strformat
+import std/sequtils
 from std/strutils import join
+
+type
+    Team = object
+        id: Natural
+        name: string
 
 type
     Hero = object
         name: string
-        team: seq[string]
+        teams: seq[Team]
         powers: seq[string]
 
+let team_xmen = Team(id: 1, name: "X-Men")
+let team_avengers = Team(id: 2, name: "Avengers")
+
 let heroes = [
-    Hero(name: "Cyclops", team: @["X-Men"], powers: @["optic blasts"]),
-    Hero(name: "Marvel Girl", team: @["X-Men"], powers: @["telepathy", "telekinesis"]),
-    Hero(name: "Beast", team: @["X-Men", "Avengers"], powers: @["enhanced strength", "enhanced agility"]),
+    Hero(name: "Cyclops", teams: @[team_xmen], powers: @["optic blasts"]),
+    Hero(name: "Marvel Girl", teams: @[team_xmen], powers: @["telepathy", "telekinesis"]),
+    Hero(name: "Beast", teams: @[team_xmen, team_avengers], powers: @["enhanced strength", "enhanced agility"]),
 ]
 
 for hero in heroes:
-    let team = hero.team.join(", ")
+    let teams = map(hero.teams, proc(t: Team): string = t.name).join(", ") # BROKEN
     let powers = hero.powers.join(" and ")
-    echo(fmt"• {hero.name} ({team}) has {powers}.")
+    echo(fmt"• {hero.name} ({teams}) has {powers}.")
