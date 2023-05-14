3fe5b8d900cb726e254c4694a90a789408743d8a
CodeConscious <hello@codeconscio.us>
2023-05-14 20:27:11 +0900

    Move guessing game to a new file

---
+++ rust/src/guessing_game.rs
@@ -0,0 +1,53 @@
+use std::io;
+use std::io::Write;
+use rand::Rng;
+
+pub fn play(show_hints: bool) {
+    let range_floor: i8 = 1;
+    let range_ceiling: i8 = 100;
+
+    print!("\x1B[2J"); // Clears the screen.
+    println!("HOW TO PLAY:");
+    println!(
+        "　{}から{}までで、好きな数字を入力して、",
+        range_floor, range_ceiling
+    );
+    println!("　ランダムに決められた『シークレットナンバー』を");
+    println!("　当ててみてください。");
+
+    let random_number = rand::thread_rng().gen_range(range_floor..=range_ceiling);
+    if show_hints {
+        println!("(The number is {}.)", random_number);
+    }
+
+    let mut attempts = 0;
+
+    loop {
+        let mut guess = String::new(); // Must be within this loop.
+        print!("＞ ");
+        io::stdout().flush().unwrap(); // The previous one will not display immediately with this.
+        std::io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read from stdin");
+
+        // let guess: i16 = guess.trim().parse().expect("Please type a number!");
+        let guess: i8 = match guess.trim().parse() {
+            // Reinstantiation!
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        // println!("You entered {}.", guess);
+
+        match guess.cmp(&random_number) {
+            std::cmp::Ordering::Less => println!("低すぎるぞ"),
+            std::cmp::Ordering::Greater => println!("高すぎるぞ"),
+            std::cmp::Ordering::Equal => {
+                println!("ピンポ〜ン！おめでとうございます!🎉");
+                println!("{}回で当てました。", attempts);
+                break;
+            }
+        }
+
+        attempts = attempts + 1;
+    }
+}
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -10,14 +10,12 @@ mod rectangle;
 mod regex;
 mod scoping;
 mod boxes;
+mod guessing_game;
 
-use rand::Rng;
 use rectangle::*;
 // use lang_test::language::{self, Language};
 // use lang_test::char_types::{JapaneseCharType, EnglishCharType};
 use lang_test::{char_types::*, language::*};
-use std::io;
-use std::io::Write;
 
 fn main() {
     // misc();
@@ -25,10 +23,10 @@ fn main() {
     // scoping::run();
     // options::run();
     // rectangle_practice();
-    // guessing_game(false);
+    // guessing_game::play(false);
     // collections::run();
     // regex::run();
-    boxes::with_and_without_boxing();
+    // boxes::with_and_without_boxing();
 }
 
 // Where unfiled code lives.
@@ -117,56 +115,6 @@ fn rectangle_practice() {
 
 fn combine_strings(first: &str, second: &str) -> String {
     first.to_owned() + second
-}
-
-fn guessing_game(show_hints: bool) {
-    let range_floor: i8 = 1;
-    let range_ceiling: i8 = 100;
-
-    print!("\x1B[2J"); // Clears the screen.
-    println!("HOW TO PLAY:");
-    println!(
-        "　{}から{}までで、好きな数字を入力して、",
-        range_floor, range_ceiling
-    );
-    println!("　ランダムに決められた『シークレットナンバー』を");
-    println!("　当ててみてください。");
-
-    let random_number = rand::thread_rng().gen_range(range_floor..=range_ceiling);
-    if show_hints {
-        println!("(The number is {}.)", random_number);
-    }
-
-    let mut attempts = 0;
-
-    loop {
-        let mut guess = String::new(); // Must be within this loop.
-        print!("＞ ");
-        io::stdout().flush().unwrap(); // The previous one will not display immediately with this.
-        std::io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read from stdin");
-
-        // let guess: i16 = guess.trim().parse().expect("Please type a number!");
-        let guess: i8 = match guess.trim().parse() {
-            // Reinstantiation!
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-        // println!("You entered {}.", guess);
-
-        match guess.cmp(&random_number) {
-            std::cmp::Ordering::Less => println!("低すぎるぞ"),
-            std::cmp::Ordering::Greater => println!("高すぎるぞ"),
-            std::cmp::Ordering::Equal => {
-                println!("ピンポ〜ン！おめでとうございます!🎉");
-                println!("{}回で当てました。", attempts);
-                break;
-            }
-        }
-
-        attempts = attempts + 1;
-    }
 }
 
 fn references() {
