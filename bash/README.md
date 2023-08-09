7aae145c4990ccc2935cfb1e28094d28de8bfeb7
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2023-08-09 20:17:39 +0900

    Add bash (#7)
    * Add readme
    
    * First commit
    
    * Update shebang

---
+++ bash/README.md
@@ -0,0 +1,3 @@
+# bash
+
+Imagine my surprise when I realized I'd forgotten about bash! So, now I have a bash playground as well.
---
+++ bash/testing.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+echo "Your kernel version is $(uname -r)."
+echo {a..j}{0..9} | tr [a..z] [A..Z] | tr 0 _
+
+greeting=こんにちは
+echo ${greeting/は/わ}
+greeting=こんばんわ
+echo ${greeting//ん/う}
+echo ${greeting:4}
