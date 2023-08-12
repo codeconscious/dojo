ae06944d19b4c7f69704f912c0460ef1ec2d78ae
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2023-08-12 15:47:45 +0900

    Cleanup

--- fsharp/src/App/Program.fs
+++ fsharp/src/App/Program.fs
@@ -3,21 +3,16 @@ open Library
 
 [<EntryPoint>]
 let main args =
-    printfn "F#アプリを実行したぜ！"
+    printfn "F#のアプリを実行したぜ！"
     printfn "System.Text.Json output regarding args:"
 
     let value, json = getJson {| args=args; year=System.DateTime.Now.Year |}
     printfn $"Input: %0A{value}"
     printfn $"Output: %s{json}"
 
-    let tens1 = everyTen [0..99]
-    printfn "Tens: %A" tens1
-
-    let tens2 = every [0..99] 10
-    printfn "Tens: %A" tens2
-
-    let sevens = every [0..99] 7
-    printfn "Sevens: %A" sevens
+    printfn "Tens: %A" (everyTen [0..99])
+    printfn "Tens: %A" (every [0..99] 10)
+    printfn "Sevens: %A" (every [0..99] 7)
 
     let sumTo30 = sumTo 30
     printfn "Tuples: %A" sumTo30
