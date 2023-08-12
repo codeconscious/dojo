fcadd0c3cebecfb53bb4a8a13cf7502f9a39b390
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2023-08-12 15:43:53 +0900

    List and tuple practice

--- fsharp/src/App/Program.fs
+++ fsharp/src/App/Program.fs
@@ -3,11 +3,24 @@ open Library
 
 [<EntryPoint>]
 let main args =
-    printfn "F#アプラを実行したぜ！"
+    printfn "F#アプリを実行したぜ！"
     printfn "System.Text.Json output regarding args:"
 
     let value, json = getJson {| args=args; year=System.DateTime.Now.Year |}
     printfn $"Input: %0A{value}"
     printfn $"Output: %s{json}"
+
+    let tens1 = everyTen [0..99]
+    printfn "Tens: %A" tens1
+
+    let tens2 = every [0..99] 10
+    printfn "Tens: %A" tens2
+
+    let sevens = every [0..99] 7
+    printfn "Sevens: %A" sevens
+
+    let sumTo30 = sumTo 30
+    printfn "Tuples: %A" sumTo30
+    printfn "Tuples summed: %A" (sumTuples sumTo30)
 
     0
--- fsharp/src/Library/Library.fs
+++ fsharp/src/Library/Library.fs
@@ -5,3 +5,21 @@ open System.Text.Json
 let getJson value =
     let json = JsonSerializer.Serialize(value)
     value, json
+
+let everyTen integers =
+    integers
+    |> List.filter(fun i -> i % 10 = 0)
+
+let every integers by =
+    integers
+    |> List.filter(fun i -> i % by = 0)
+
+let sumTo target =
+        [ for i in 1..99 do
+            for j in 1..99 do
+                if (i + j = target) then
+                    yield (i, j) ]
+
+let sumTuples tuples =
+    tuples
+    |> List.map(fun (x,y) -> x + y)
