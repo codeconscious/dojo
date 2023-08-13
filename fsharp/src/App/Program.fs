6ae3fbeb5a092fc3a16c285a9e0643afb466eade
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2023-08-13 10:42:42 +0900

    Pipelines and composition

--- fsharp/src/App/Program.fs
+++ fsharp/src/App/Program.fs
@@ -2,38 +2,46 @@
 open Library
 
 module Basics =
-    printfn "F#のアプリを実行したぜ！"
-    printfn "System.Text.Json output regarding args:"
-
+    // printfn "System.Text.Json output regarding args:"
     let args = [|""|]
     let value, json = getJson {| args=args; year=System.DateTime.Now.Year |}
-    printfn $"Input: %0A{value}"
-    printfn $"Output: %s{json}"
+    // printfn $"Input: %0A{value}"
+    // printfn $"Output: %s{json}"
 
-    printfn "Tens: %A" (everyTen [0..99])
-    printfn "Tens: %A" (every [0..99] 10)
-    printfn "Sevens: %A" (every [0..99] 7)
+    // printfn "Tens: %A" (everyTen [0..99])
+    // printfn "Tens: %A" (every [0..99] 10)
+    // printfn "Sevens: %A" (every [0..99] 7)
 
     let sumTo30 = sumTo 30
-    printfn "Tuples: %A" sumTo30
-    printfn "Tuples summed: %A" (sumTuples sumTo30)
+    // printfn "Tuples: %A" sumTo30
+    // printfn "Tuples summed: %A" (sumTuples sumTo30)
 
 module Functions =
     // Higher-order functions
     let powerLevel = 100
     let addTen i = i + 10
     let applyAndDouble (fn:int -> int) i = fn i * 2
-    printfn "Final: %i" (applyAndDouble addTen powerLevel)
+    // printfn "Final: %i" (applyAndDouble addTen powerLevel)
 
     // Partial application of arguments
     let areBothTrue x y = x && y
     let testPartial = areBothTrue true
-    printfn "%b" (testPartial true)
-    printfn "%b" (testPartial false)
+    // printfn "%b" (testPartial true)
+    // printfn "%b" (testPartial false)
 
     // Recursive
     let rec fib n = if n < 2 then 1 else fib(n-1) + fib(n-2)
-    printfn "%i" (fib 10)
+    // printfn "%i" (fib 10)
+
+module FunctionComposition =
+    let getRandom(pool:list<string>) =
+        let rnd = Random()
+        pool
+        |> List.map(fun x -> x.Replace('は', 'ズ'))
+        |> List.item(rnd.Next(pool.Length))
+    let encrypt(word:string) =
+        word |> String.map(fun s -> '*')
+    let randomEncrypted = getRandom >> encrypt
 
 module People =
     open MyTypes
@@ -58,11 +66,20 @@ module People =
         ororo)
     // let moira = Person("Moria", "MacTaggert", (uint8) 43)
     let xmen = MyTypes.Team("X-Men", [|cyclops; phoenix; storm|])
-    printfn "%s" xmen.Describe
-    printfn "%s" cyclops.Describe
-    printfn "%s" phoenix.Describe
-    printfn "%s" storm.Describe
+    // printfn "%s" xmen.Describe
+    // printfn "%s" cyclops.Describe
+    // printfn "%s" phoenix.Describe
+    // printfn "%s" storm.Describe
 
-[<EntryPoint>]
-let main args =
-    0
+// [<EntryPoint>]
+// let main args =
+//     open FunctionComposition
+//     printfn "F#のアプリを実行したぜ！"
+//     0
+
+module Run =
+    open FunctionComposition
+    let words = ["おはようございます"; "こんにちは"; "こんばんは"]
+    printfn "Random greeting: %s" (getRandom words)
+    printfn "Encrypted word: %s" (encrypt "hello")
+    printfn "Random encrypted word: %s" (randomEncrypted words)
