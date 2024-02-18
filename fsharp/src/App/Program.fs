1abb4bf43e5b2dd490da4ef1e36434874f7f28b9
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2024-02-18 10:11:36 +0900

    Nomenclature and such

--- fsharp/src/App/Program.fs
+++ fsharp/src/App/Program.fs
@@ -28,6 +28,6 @@ module Run =
     // printfn "%s" (quad 3 7 10)
     // printfn "%s" (quad -1 50 50)
     // printfn "%s" (quad 50 100 50)
-    printfn "%s" (quad 1 3 1) // Two: -0.381966 and -2.618034
-    printfn "%s" (quad 1 2 1) // One: -1.000000
-    printfn "%s" (quad 1 2 3) // None
+    printfn "%s" (calculate 1 3 1) // Two: -0.381966 and -2.618034
+    printfn "%s" (calculate 1 2 1) // One: -1.000000
+    printfn "%s" (calculate 1 2 3) // None
--- fsharp/src/App/Quadratic.fs
+++ fsharp/src/App/Quadratic.fs
@@ -1,22 +1,22 @@
 module Quadratic
-    type EquationResult =
+    type EquationType =
         | NotQuadratic
         | NoRoots
         | OneRoot of positive : float
         | TwoRoots of positive : float * negative : float
 
-    let quad a b c =
+    let calculate a b c =
         let discrimant =
             if a = 0.0 then None
             else Some((pown b 2) - (4.0 * a * c)); // b^2 - 4ac
-        let rootPos discrimant a b = (-b + sqrt discrimant) / (2.0 * a); // quad eq plus sqrt
-        let rootNeg discrimant a b = (-b - sqrt discrimant) / (2.0 * a); // quad eq minus sqrt
+        let rootPos discrimant a b = (-b + sqrt discrimant) / (2.0 * a);
+        let rootNeg discrimant a b = (-b - sqrt discrimant) / (2.0 * a);
 
         let result = match discrimant with
-                          | None -> NotQuadratic
-                          | Some d when d < 0 -> NoRoots
-                          | Some d when d = 0 -> OneRoot (rootPos d a b)
-                          | Some d -> TwoRoots((rootPos d a b), (rootNeg d a b))
+                     | None -> NotQuadratic
+                     | Some d when d < 0 -> NoRoots
+                     | Some d when d = 0 -> OneRoot (rootPos d a b)
+                     | Some d -> TwoRoots((rootPos d a b), (rootNeg d a b))
 
         match result with
         | NotQuadratic -> "The equation is not quadratic because `a` is zero!"
