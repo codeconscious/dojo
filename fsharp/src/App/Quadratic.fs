895c08834a41acee79ed4d5c16bb7dc4ecfbd687
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2024-02-19 12:43:50 +0900

    Remove comment

--- fsharp/src/App/Quadratic.fs
+++ fsharp/src/App/Quadratic.fs
@@ -8,7 +8,7 @@ module Quadratic
     let calculate a b c =
         let discrimant =
             if a = 0.0 then None
-            else Some((pown b 2) - (4.0 * a * c)); // b^2 - 4ac
+            else Some((pown b 2) - (4.0 * a * c));
         let rootPos discrimant a b = (-b + sqrt discrimant) / (2.0 * a);
         let rootNeg discrimant a b = (-b - sqrt discrimant) / (2.0 * a);
 
@@ -19,7 +19,7 @@ module Quadratic
                      | Some d -> TwoRoots((rootPos d a b), (rootNeg d a b))
 
         match result with
-        | NotQuadratic -> "The equation is not quadratic because `a` is zero!"
+        | NotQuadratic -> "The equation is not quadratic (because `a` is zero)!"
         | NoRoots -> "The equation has no real roots."
         | OneRoot r -> sprintf "The equation has one root: %f." r
         | TwoRoots (r1, r2) -> sprintf "The equation has two roots: %f and %f." r1 r2
