888a413d65fb2739d049b403ef85bd5079474ac8
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2024-02-21 18:13:13 +0900

    Remove unnecessary array

--- fsharp/src/App/Dice.fs
+++ fsharp/src/App/Dice.fs
@@ -1,6 +1,7 @@
 module Dice
 
 type Die = Die of list<int>
+
 // Original manual ctor:
 // let createDie max = Die (Seq.toList { 1 .. max })
 
@@ -17,8 +18,7 @@ let private roll (Die d) = d[rnd.Next(d.Length - 1)]
 
 let rollPrintMultiple times die =
     seq {for _ in 1..times do yield roll die}
-    |> Seq.toArray
-    |> Array.map (fun i -> i.ToString())
+    |> Seq.map (fun i -> i.ToString())
     |> String.concat " and "
     |> printfn "You rolled %s."
 
