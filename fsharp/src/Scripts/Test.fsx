75df5f6eed290e969316cffbd5fa353af7e16a71
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2024-03-19 18:07:28 +0900

    Minor tweaks

--- fsharp/src/Scripts/Test.fsx
+++ fsharp/src/Scripts/Test.fsx
@@ -1,5 +1,7 @@
+// In full projects, perhaps `System.Environment.GetCommandLineArgs() is equivalent.
 let args = fsi.CommandLineArgs |> Array.tail // The head is the filename
+
 match args with
 | a when a.Length = 0 -> "none"
-| a -> String.concat "\" and \"" a
-|> printfn "Argument(s): \"%s\""
+| a -> String.concat "; " a
+|> printfn "Argument(s): %s"
