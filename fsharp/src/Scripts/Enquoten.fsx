68a00827de546f42ac1b6731580d92812ac4d421
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2024-05-04 16:21:22 +0900

    Allow custom quote prefixes

--- fsharp/src/Scripts/Enquoten.fsx
+++ fsharp/src/Scripts/Enquoten.fsx
@@ -1,26 +1,26 @@
-(* ENQUOTEN F# Script
+(* Enquoten: An F# Script
 
-   Summary: Reads a text-only file and splits its lines to the maximum line length
-            limit provided. Each line is prefixed with "> ", and the prefix is included
-            in the line length calculation.
+   Summary: Reads a text file and splits its lines to the maximum line length
+            limit provided. The prefix is included in the line length calculation, and it
+            must be shorter then the line length limit. The script will try to split lines
+            at spaces, but if none are found, it will cut the line at the line length limit.
 
-   Requirements: .NET 8 SDK (Untested on previous versions, though it might work)
+   Requirements: .NET 8 runtime (Untested on previous versions, though it might work)
 
-   Usage: dotnet fsi <lineLengthLimit> <filePath(s)>
-          Sample: dotnet fsi 70 'Documents/file1.txt'
+   Usage: dotnet fsi <lineLengthLimit> <quotePrefix> <filePath>
+          Sample: dotnet fsi 70 "> " file1.txt
 
    Note: This was created mainly for learning purposes and might not be very good. ^_^
 
-   Last update: May 4, 2024, to add computation expressions
+   Last significant update: May 4, 2024, to add computation expressions and allow custom prefixes.
 *)
 
 open System
 open System.IO
 
-let quotePrefix = "> "
-
 type Args<'T> =
     { Limit: 'T
+      Prefix: string
       File: string }
 
 type ResultBuilder() =
@@ -42,18 +42,27 @@ let validateArgs =
 
     let validateArgCount (args:string list) =
         match args.Length with
-        | l when l = 2 ->
-            Ok { Limit = args.Head; File = args.Tail.Head }
+        | l when l = 3 ->
+            Ok { Limit = args.Head; Prefix = args[1]; File = args[2] }
         | _ ->
-            Error "You must supply a maximum line length and one file."
+            Error "You must supply a maximum line length, a quote prefix (such as '> '), and one file path."
 
     let validateLimit (args:Args<string>) =
         match args with
-        | { Args.Limit = l; Args.File = f } ->
+        | { Args.Limit = l; Prefix = p; Args.File = f } ->
             match (l |> System.Int32.TryParse) with
-            | true, i when i >= 10 -> Ok { Limit = i; File = f }
+            | true, i when i >= 2 -> Ok { Limit = i; Prefix = p; File = f }
             | true, _ -> Error "Requested line length too short."
             | _ -> Error "Maximum line length must be numeric."
+
+    let validatePrefix (args:Args<int>) =
+        match args with
+        | { Prefix = p; Limit = l } when p.Length < l ->
+            Ok args
+        | _ ->
+            Error (sprintf "The prefix text length (%i) must be at least 1 character less than the line length limit (%i)."
+                args.Prefix.Length
+                args.Limit)
 
     let validateFileExists (args:Args<int>) =
         match args with
@@ -65,8 +74,9 @@ let validateArgs =
     result {
         let! args = validateArgCount rawArgs
         let! args' = validateLimit args
-        let! args'' = validateFileExists args'
-        return args''
+        let! args'' = validatePrefix args'
+        let! args''' = validateFileExists args''
+        return args'''
     }
 
 let readFile file =
@@ -78,42 +88,41 @@ let readFile file =
         | :? FileNotFoundException -> Error $"\"{file}\" was not found."
         | e -> Error $"Unexpectedly could not read \"{file}\": {e.Message}"
 
-let splitLine fullLine lengthLimit =
+let splitLine fullLine (args:Args<int>) =
     let finalSpaceIndex (text:string) (indexCeiling:int) =
-        let resultIndex = text.LastIndexOf(" ", indexCeiling) // Searches backwards
+        let index = text.LastIndexOf(" ", indexCeiling) // Searches backwards
         match text.LastIndexOf(" ", indexCeiling) with
         | -1 -> indexCeiling - 1
-        | _  -> resultIndex
+        | _  -> index
 
     let rec loop acc (linePart:string) limit =
         match linePart.Length with
-        | l when l <= limit -> acc @ [linePart]
+        | l when l <= limit ->
+            acc @ [linePart]
         | _ ->
             let splitIndex = finalSpaceIndex linePart limit
             let trimmed = linePart[..splitIndex]
             let remaining = linePart[splitIndex+1..]
             loop (acc @ [trimmed]) remaining limit
-    loop [] fullLine lengthLimit
+    loop [] fullLine <| args.Limit - args.Prefix.Length
 
 let enquoten prefix text =
     text |> sprintf "%s%s" prefix
 
-let processFileText limit (text:string) =
+let processFileText (args:Args<int>) (text:string) =
     text.Split Environment.NewLine
     |> Array.toList
-    |> List.map (fun l -> splitLine l limit)
+    |> List.map (fun l -> splitLine l args)
     |> List.collect (fun l -> l)
-    |> List.map (fun l -> enquoten quotePrefix l)
+    |> List.map (fun l -> enquoten args.Prefix l)
 
 let output =
     result {
         let! okArgs = validateArgs
         let! fileText = readFile okArgs.File
-        return fileText |> processFileText okArgs.Limit
+        return fileText |> processFileText okArgs
     }
 
 match output with
-| Ok lines ->
-    lines |> List.iter (fun l -> l |> printfn "%s")
-| Error e ->
-    e |> printfn "%s"
+| Ok lines -> lines |> List.iter (fun l -> l |> printfn "%s")
+| Error e -> e |> printfn "%s"
