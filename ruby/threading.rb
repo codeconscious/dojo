5e1c998110d3e0837c47472046349df3dfc41dee
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2024-06-23 22:25:34 +0900

    Add error-handling

--- ruby/threading.rb
+++ ruby/threading.rb
@@ -1,17 +1,23 @@
-require 'net/http'
+require 'net/http' # Also auto-`require`s `uri`
 
 pages = %w[ www.codeconscio.us
             www.github.com
-            www.duckduckgo.com ]
+            www.duckduckgo.com
+            www.wikipedia.com
+            www.wildly-invalid.co.jp ]
 
 threads = []
 
 pages.each do |page|
   threads << Thread.new(page) do |this_page|
     http = Net::HTTP.new(this_page, 80)
-    puts "Fetching: #{this_page}"
-    resp, = http.get('/', nil) # Discards the data
-    puts "Got #{this_page}:  #{resp.message}"
+    puts "• Fetching: #{this_page}"
+    begin
+      resp, = http.get('/', nil) # Discards the data
+      puts "✔︎ Got #{this_page}: #{resp.code} (#{resp.message})"
+    rescue SocketError
+      puts "✖︎ Failed to retrieve #{this_page}"
+    end
   end
 end
 
