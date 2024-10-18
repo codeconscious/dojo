cd168a14542bfa257ce1b34217aa1d01a01b408a
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2024-10-18 12:44:02 +0900

    Add web server

---
+++ go/server.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func learnWebProgramming() {
+	// First parameter of ListenAndServe is TCP address to listen to.
+	// Second parameter is an interface, specifically http.Handler.
+	go func() {
+		err := http.ListenAndServe(":8080", nil)
+		fmt.Println(err) // don't ignore errors
+	}()
+
+	requestServer()
+}
+
+func requestServer() {
+	resp, err := http.Get("http://localhost:8080")
+	fmt.Println(err)
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	fmt.Printf("\nWebserver said: `%s`", string(body))
+}
