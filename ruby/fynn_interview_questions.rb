e8a44b579527fe0660de85837d17603135bba49d
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2025-10-25 19:47:19 +0900

    Use classes instead of modules

--- ruby/fynn_interview_questions.rb
+++ ruby/fynn_interview_questions.rb
@@ -5,35 +5,39 @@ def ensure_equal(expected, actual)
   puts "NOT EQUAL! Expected: #{expected} / Actual: #{actual}" if actual != expected
 end
 
-module Easy
-  def self.one
-    input = %w[this is an array]
-    expected = [4, 2, 2, 5]
-    actual = input.map(&:length)
-    ensure_equal actual, expected
-  end
+class Easy
+  class << self
+    def one
+      input = %w[this is an array]
+      expected = [4, 2, 2, 5]
+      actual = input.map(&:length)
+      ensure_equal actual, expected
+    end
 
-  def self.five
-    input = 'abccba'
-    expected = true
-    actual = input.reverse == input
-    ensure_equal actual, expected
-  end
+    def five
+      input = 'abccba'
+      expected = true
+      actual = input.reverse == input
+      ensure_equal actual, expected
+    end
 
-  def self.nine
-    input = %w[fynn nyfn]
-    expected = true
-    actual = input.map { |word| word.chars.sort }.uniq.length == 1
-    ensure_equal actual, expected
+    def nine
+      input = %w[fynn nyfn]
+      expected = true
+      actual = input.map { |word| word.chars.sort }.uniq.length == 1
+      ensure_equal actual, expected
+    end
   end
 end
 
-module Medium
-  def self.eight
-    input = [[3, 4, 1, 2], [9, 4, 8, 2]]
-    expected = 70 # Abbreviated output, eliding the text output in the original problem.
-    actual = input.map(&:sort).transpose.map { |x| x.reduce(:*) }.sum
-    ensure_equal expected, actual
+class Medium
+  class << self
+    def eight
+      input = [[3, 4, 1, 2], [9, 4, 8, 2]]
+      expected = 70 # Abbreviated output, eliding the text output in the original problem.
+      actual = input.map(&:sort).transpose.map { |x| x.reduce(:*) }.sum
+      ensure_equal expected, actual
+    end
   end
 end
 
