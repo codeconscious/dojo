789b0838d2b2c8963954eed3810952daa2d201da
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2025-10-26 12:55:08 +0900

    Resolve warnings, including use of `Control.Monad.when`

--- haskell/scripts/FynnInterviewQuestions.Medium.hs
+++ haskell/scripts/FynnInterviewQuestions.Medium.hs
@@ -1,19 +1,20 @@
+{-# OPTIONS_GHC -Wall -Werror #-}
+
 -- Haskell doesn't support multiple or nested modules!
 module FynnInterviewQuestions.Medium where
 
 import Data.List (transpose, sort)
 import Data.Function ((&))
+import qualified Control.Monad
 
 ensureEqual :: (Eq a, Show a) => a -> a -> IO ()
 ensureEqual actual expected =
-  if actual /= expected
-    then print $ "NOT EQUAL! Actual: " ++ show actual ++ " / Expected: " ++ show expected
-    else return ()
+  Control.Monad.when (actual /= expected) $ putStrLn $ "NOT EQUAL! Actual: " ++ show actual ++ " / Expected: " ++ show expected
 
 eight :: IO ()
 eight = do
-  let input = [ [3, 4, 1, 2], [9, 4, 8, 2] ]
-  let expected = 70
+  let input = [ [3, 4, 1, 2], [9, 4, 8, 4] ]
+  let expected = (70 :: Integer)
   ensureEqual expected $ input & map sort & transpose & map product & sum
 
 main :: IO ()
