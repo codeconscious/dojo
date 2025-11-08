f216ef95a911dc2a215828da9c2afd3fadadccee
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2025-11-08 18:21:08 +0900

    Add sum example

--- ocaml/sandbox2/sandbox2/bin/main.ml
+++ ocaml/sandbox2/sandbox2/bin/main.ml
@@ -1,1 +1,13 @@
-let () = print_endline "Hello, World!"
+(* let () = print_endline "Hello, World!" *)
+
+open Base
+open Stdio
+
+let rec read_and_accumulate accum =
+  let line = In_channel.input_line In_channel.stdin in
+  match line with
+  | None -> accum
+  | Some x -> read_and_accumulate (accum +. Float.of_string x)
+
+let () =
+  printf "Total: %F\n" (read_and_accumulate 0.) (* You must press Ctrl-D to finish. *)
