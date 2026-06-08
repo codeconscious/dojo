328c78616b05037cf5d8aaa377701480889245d1
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2026-06-08 18:33:00 +0900

    Formatting

--- haskell/scripts/HaskellForImperativeProgrammers/11_FoldExercises.hs
+++ haskell/scripts/HaskellForImperativeProgrammers/11_FoldExercises.hs
@@ -5,5 +5,5 @@ import Data.Traversable
 -- Reverse a list using a fold.
 -- https://youtu.be/46dksIrx6jQ?list=PLe7Ei6viL6jGp1Rfu0dil1JH1SHk9bgDV
 rev' :: [a] -> [a]
-rev' xs = foldl' (\acc x -> x:acc) [] xs
--- rev' = foldl' (\acc x -> x:acc) []
+rev' xs = foldl' (\acc x -> x : acc) [] xs
+-- rev' = foldl' (\acc x -> x : acc) []
